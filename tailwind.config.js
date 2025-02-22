49f775abfdbd4cd6864e6c91ec73372784190753
younixue3 <bussiness.needs.rctka@gmail.com>
2025-02-22 12:32:08 +0800

    config(taiwlind): implement prefix for tailwind css utility class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: ["../../custom_TA/**/*.{html,js,xml}"], // Make sure to include your custom addons' paths here as well
   theme: {
-  extend: {},
+    extend: {},
   },
   plugins: [],
-  
-  blocklist: ['container', 'collapse'], // Add classes to blocklist to avoid generating them
-  }
+  prefix: "tw-",
+  blocklist: ["container", "collapse"], // Add classes to blocklist to avoid generating them
+};
